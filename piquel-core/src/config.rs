3fabc780baa34751028e702964e29878c6d5cabd
PiquelChips <piquel@piquel.fr>
2026-02-19 21:32:54 +0100

    update socket path

--- piquel-core/src/config.rs
+++ piquel-core/src/config.rs
@@ -1,2 +1,3 @@
-pub const SOCKET_PATH: &str = "/run/piqueld.sock";
+// TODO: rename to "/run/piqueld.sock" when we run as root
+pub const SOCKET_PATH: &str = "/tmp/piqueld.sock";
 pub const LISTEN_ADDR: &str = "0.0.0.0:7854";
