ca373fd83d67a01c4857eb9cf88cd9e92ad42fcf
PiquelChips <piquel@piquel.fr>
2026-02-19 21:33:02 +0100

    create client implementation

--- piquel-core/src/ipc/client.rs
+++ piquel-core/src/ipc/client.rs
@@ -1,5 +1,62 @@
-trait Client {}
+use crate::config::{LISTEN_ADDR, SOCKET_PATH};
 
-struct TcpClient {}
+use super::message::{Command, Response};
+use std::{error::Error, net::TcpStream, os::unix::net::UnixStream};
 
-struct UnixServer {}
+pub enum ClientType {
+    TcpClient,
+    UdsClient,
+}
+
+pub trait Client {
+    fn run_command(&self, command: &Command) -> Result<Response, Box<dyn Error>>;
+    fn get_type(&self) -> ClientType;
+}
+
+pub struct TcpClient {
+    pub stream: TcpStream,
+}
+
+impl TcpClient {
+    pub fn new() -> Result<TcpClient, bool> {
+        let stream = match TcpStream::connect(LISTEN_ADDR) {
+            Ok(tcp_stream) => tcp_stream,
+            Err(_) => return Err(false),
+        };
+
+        Ok(TcpClient { stream })
+    }
+}
+
+impl Client for TcpClient {
+    fn get_type(&self) -> ClientType {
+        ClientType::TcpClient
+    }
+    fn run_command(&self, command: &Command) -> Result<Response, Box<dyn Error>> {
+        Result::Ok(Response::Ok)
+    }
+}
+
+pub struct UdsClient {
+    pub stream: UnixStream,
+}
+
+impl UdsClient {
+    pub fn new() -> Result<UdsClient, bool> {
+        let stream = match UnixStream::connect(SOCKET_PATH) {
+            Ok(tcp_stream) => tcp_stream,
+            Err(_) => return Err(false),
+        };
+
+        Ok(UdsClient { stream })
+    }
+}
+
+impl Client for UdsClient {
+    fn get_type(&self) -> ClientType {
+        ClientType::UdsClient
+    }
+    fn run_command(&self, command: &Command) -> Result<Response, Box<dyn Error>> {
+        Result::Ok(Response::Ok)
+    }
+}
