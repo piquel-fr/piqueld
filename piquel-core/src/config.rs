e3176aa1528c7cf6b9197137364ddbdb708c3e64
PiquelChips <piquel@piquel.fr>
2026-03-04 10:29:50 +0100

    start adding to config

--- piquel-core/src/config.rs
+++ piquel-core/src/config.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+use serde::{Deserialize, Serialize};
+
 // TODO: rename to "/run/piqueld.sock" when we run as root
 pub const SOCKET_PATH: &str = "/tmp/piqueld.sock";
 pub const LISTEN_ADDR: &str = "0.0.0.0:7854";
+
+pub const DATA_DIR: &str = "/var/lib/piqueld";
+
+/// Settings for how to load configuration. This is mainly to accomodate for
+/// server and CLI configuration.
+///
+///
+pub struct ConfigLoadSetting {}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {}
+
+impl Config {
+    fn load() -> io::Result<Self> {
+        Ok(Config {})
+    }
+}
