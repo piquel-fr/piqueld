e569453fffdf431a5449d34ab03991ea6df1a0af
Piquel <piquel@piquel.fr>
2026-03-06 09:51:31 +0100

    refactor: use tokio in server (#8)
    Use Tokio async runtime to handle connections and listening.

--- piqueld/src/main.rs
+++ piqueld/src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 
 use piquelcore::ipc::server::Server;
 
-fn main() -> io::Result<()> {
-    Server::new().listen()
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    Server::new().listen().await
 }
