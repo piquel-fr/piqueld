eeeb7553d9ba049b4b2baad9648fbc72f007ae4a
Piquel <piquel@piquel.fr>
2026-03-06 22:04:02 +0100

    Add Nix module configuration (#9)
    Closes #3
    
    Add flake with:
    - NixOS module with both ctl & daemon configuration
    - Export binary derivations for both daemon & ctl
    - Devshell with ctl

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+    description = "piqueld";
+    
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    
+    outputs = { self, nixpkgs, flake-utils }: 
+    {
+        nixosModules.default = import ./nix/module.nix;
+    } //
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+            inherit (self) outputs;
+            pkgs = import nixpkgs {inherit system;};
+        in
+        {
+            packages = import ./nix/pkgs.nix { inherit pkgs; };
+            devShells.default = import ./nix/shell.nix { inherit outputs system pkgs; };
+        }
+    );
+}
---
+++ nix/module.nix
@@ -0,0 +1,132 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  piquelpkgs = (import ./pkgs.nix { inherit pkgs; });
+  piqueld = piquelpkgs.piqueld;
+
+  cfg = config.services.piqueld;
+  settingsFormat = pkgs.formats.json { };
+
+  ctlConfig = settingsFormat.generate "piquelctl.json" cfg.ctlSettings;
+  daemonConfig = settingsFormat.generate "piqueld.json" cfg.daemonSettings;
+
+  piquelctl =
+    pkgs.runCommand "piquelctl-wrapped"
+      {
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      }
+      ''
+        makeWrapper ${piquelpkgs.piquelctl}/bin/piquelctl $out/bin/piquelctl \
+            --add-flags "--config ${ctlConfig}"
+      '';
+in
+{
+  options.services.piqueld = {
+    enable = lib.mkEnableOption "piquelctl";
+
+    # CTL
+
+    ctlPackage = lib.mkOption {
+      type = lib.types.package;
+      default = piquelctl;
+    };
+
+    ctlSettings = lib.mkOption {
+      description = "The configuration passed to the control cli";
+      default = {};
+      type = lib.types.submodule {
+        freeformType = settingsFormat.type;
+        options =
+          let
+            inherit (lib) mkOption types;
+          in
+          {
+            socket_path = mkOption {
+              type = types.str;
+              default = "/run/piqueld/piqueld.sock";
+              description = "Path to the socket";
+            };
+          };
+      };
+    };
+
+    # DAEMON
+
+    enableDaemon = lib.mkOption {
+      default = true;
+      example = false;
+      description = "Whether to enable the daemon (piqueld).";
+      type = lib.types.bool;
+    };
+
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = "piqueld";
+      description = "Group that can access the piqueld socket.";
+    };
+
+    daemonPackage = lib.mkOption {
+      type = lib.types.package;
+      default = piqueld;
+    };
+
+    daemonSettings = lib.mkOption {
+      description = "The configuration passed to the daemon";
+      default = {};
+      type = lib.types.submodule {
+        freeformType = settingsFormat.type;
+        options =
+          let
+            inherit (lib) mkOption types;
+          in
+          {
+            data_dir = mkOption {
+              type = types.str;
+              default = "/var/lib/piqueld";
+              description = "Path to daemon data";
+            };
+            socket_path = mkOption {
+              type = types.str;
+              default = "/run/piqueld/piqueld.sock";
+              description = "Path to the socket";
+            };
+            listen_addr = mkOption {
+              type = types.str;
+              default = "0.0.0.0:7854";
+              description = "Address to listen to";
+            };
+          };
+      };
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = [ cfg.ctlPackage ];
+    })
+    (lib.mkIf cfg.enableDaemon {
+      environment.etc."piqueld/config.json".source = daemonConfig;
+
+      systemd.services.piqueld = {
+        description = "piqueld";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        serviceConfig = {
+          ExecStart = "${piqueld}/bin/piqueld --config /etc/piqueld/config.json";
+          DynamicUser = true;
+          Group = cfg.group;
+          StateDirectory = "piqueld";
+          RuntimeDirectory = "piqueld";
+          RuntimeDirectoryMode = "0755";
+          PrivateTmp = true;
+          ProtectSystem = "strict";
+          ProtectHome = true;
+        };
+      };
+    })
+  ];
+}
