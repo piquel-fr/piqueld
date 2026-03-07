5e0cafb78509eb886ccd1f100b9c977d74cf32a7
PiquelChips <piquel@piquel.fr>
2026-03-07 13:04:48 +0100

    implement Display for Command

--- piquel-core/src/ipc/message.rs
+++ piquel-core/src/ipc/message.rs
@@ -9,6 +9,28 @@ pub enum Command {
     Stop,
 }
 
+impl std::fmt::Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Echo(msg) => {
+                write!(f, "Echo \"{msg}\"")
+            }
+            Self::Status => {
+                write!(f, "Status")
+            }
+            Self::Hostname => {
+                write!(f, "Hostname")
+            }
+            Self::Reload => {
+                write!(f, "Reload")
+            }
+            Self::Stop => {
+                write!(f, "Stop")
+            }
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Response {
     Ok,
