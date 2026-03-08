03049eb329233a5dad51107daf1373b8b8bcd5a7
PiquelChips <piquel@piquel.fr>
2026-03-08 17:37:09 +0100

    update GitService for better async

--- piqueld/src/git.rs
+++ piqueld/src/git.rs
@@ -1,8 +1,8 @@
-use std::{fs, io, path::PathBuf};
+use std::{fs, path::PathBuf};
 
 use gix::bstr::BString;
 use log::{info, trace};
-use tokio::sync::oneshot;
+use tokio::sync::{mpsc, oneshot};
 
 use crate::config::ServerConfig;
 
@@ -19,13 +19,59 @@ pub enum GitCommand {
     },
 }
 
-pub struct GitService {
+pub struct GitHandle {
+    tx: mpsc::Sender<GitCommand>,
+}
+
+impl GitHandle {
+    pub async fn get_repository(&self, owner: &str, repo: &str) -> piquel::Result<Repository> {
+        let (reply, rx) = oneshot::channel();
+        self.tx
+            .send(GitCommand::GetRepository {
+                owner: owner.to_string(),
+                name: repo.to_string(),
+                reply,
+            })
+            .await?;
+        rx.await?
+    }
+    pub async fn list_repositories(&self) -> piquel::Result<Vec<Repository>> {
+        let (reply, rx) = oneshot::channel();
+        self.tx.send(GitCommand::ListRepositories { reply }).await?;
+        rx.await?
+    }
+}
+
+pub fn new_git_service(config: &ServerConfig) -> GitHandle {
+    let (tx, mut rx) = mpsc::channel::<GitCommand>(32);
+
+    let service = GitService::new(&config);
+
+    tokio::spawn(async move {
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                GitCommand::GetRepository { owner, name, reply } => {
+                    let result = service.get_repository(&owner, &name);
+                    let _ = reply.send(result);
+                }
+                GitCommand::ListRepositories { reply } => {
+                    let result = service.list_repositories();
+                    let _ = reply.send(result);
+                }
+            };
+        }
+    });
+
+    GitHandle { tx }
+}
+
+struct GitService {
     path: PathBuf,
     repo_path: PathBuf,
 }
 
 impl GitService {
-    pub fn new(config: &ServerConfig) -> Self {
+    fn new(config: &ServerConfig) -> Self {
         let mut path = config.data_dir.clone();
         path.push("git");
 
@@ -37,11 +83,7 @@ impl GitService {
 
         Self { path, repo_path }
     }
-    fn get_repository(
-        &self,
-        owner: &str,
-        repo: &str,
-    ) -> Result<Repository, Box<dyn std::error::Error>> {
+    fn get_repository(&self, owner: &str, repo: &str) -> piquel::Result<Repository> {
         let mut path = self.repo_path.clone();
         path.push(repo);
 
@@ -62,11 +104,7 @@ impl GitService {
             name: repo.to_string(),
         })
     }
-    fn clone(
-        &self,
-        owner: &str,
-        repo: &str,
-    ) -> Result<gix::Repository, Box<dyn std::error::Error>> {
+    fn clone(&self, owner: &str, repo: &str) -> piquel::Result<gix::Repository> {
         let mut path = self.repo_path.clone();
         path.push(repo);
 
@@ -80,7 +118,7 @@ impl GitService {
         info!("{PREFIX} Successfully cloned {owner}/{repo}");
         Ok(repository)
     }
-    pub fn list_repositories(&self) -> Result<Vec<Repository>, Box<dyn std::error::Error>> {
+    fn list_repositories(&self) -> piquel::Result<Vec<Repository>> {
         let dir = fs::read_dir(&self.repo_path)?;
         let repos = dir
             .filter_map(Result::ok)
