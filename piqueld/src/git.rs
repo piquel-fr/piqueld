e0a43af3adf031b0660ba709d3713a53698d61bf
PiquelChips <piquel@piquel.fr>
2026-03-08 14:25:41 +0100

    setup repository cloning

--- piqueld/src/git.rs
+++ piqueld/src/git.rs
@@ -1,6 +1,11 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
+
+use gix::bstr::BString;
+use log::{info, trace};
 
 use crate::config::ServerConfig;
+
+const PREFIX: &str = "[GIT]";
 
 pub struct Git {
     path: PathBuf,
@@ -13,14 +18,62 @@ impl Git {
         path.push("git");
 
         let mut repo_path = path.clone();
-        repo_path.push("repository");
+        repo_path.push("repositories");
+
+        fs::create_dir_all(&path).expect("It don't know why this would fail");
+        fs::create_dir_all(&repo_path).expect("It don't know why this would fail");
 
         Ok(Git { path, repo_path })
     }
-    pub fn get_repository(&self) -> Result<Repository, Box<dyn std::error::Error>> {
-        let repository = gix::open(&self.repo_path)?;
+    pub fn get_repository(
+        &self,
+        owner: &str,
+        repo: &str,
+    ) -> Result<Repository, Box<dyn std::error::Error>> {
+        let mut path = self.repo_path.clone();
+        path.push(repo);
+
+        let repository = match gix::open(path) {
+            Ok(repository) => {
+                trace!("{PREFIX} Found repository {owner}/{repo} on system");
+                repository
+            }
+            Err(_) => {
+                info!("{PREFIX} Couldn't load repository {owner}/{repo}. Attempting to clone...");
+                self.clone(owner, repo)?
+            }
+        };
 
         Ok(Repository { repository })
+    }
+    fn clone(
+        &self,
+        owner: &str,
+        repo: &str,
+    ) -> Result<gix::Repository, Box<dyn std::error::Error>> {
+        let mut path = self.repo_path.clone();
+        path.push(repo);
+
+        let mut prepare_checkout = gix::prepare_clone(Git::make_repo_url(owner, repo)?, path)?
+            .fetch_then_checkout(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?
+            .0;
+
+        let repository = prepare_checkout
+            .main_worktree(gix::progress::Discard, &gix::interrupt::IS_INTERRUPTED)?
+            .0;
+        info!("{PREFIX} Successfully cloned {owner}/{repo}");
+        Ok(repository)
+    }
+    fn make_repo_url(owner: &str, repo: &str) -> Result<gix::Url, gix::url::parse::Error> {
+        gix::Url::from_parts(
+            gix::url::Scheme::Ssh,
+            Some("git".into()),
+            None,
+            Some("github.com".into()),
+            None,
+            BString::from(format!("{owner}/{repo}")),
+            false,
+        )
     }
 }
 
