1d2be589a2072cdb14155c6143545b28ac65e876
PiquelChips <piquel@piquel.fr>
2026-03-08 17:36:43 +0100

    update piquel::Result for async

--- piquelcore/src/lib.rs
+++ piquelcore/src/lib.rs
@@ -2,4 +2,4 @@ pub mod config;
 pub mod ipc;
 pub mod logging;
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
