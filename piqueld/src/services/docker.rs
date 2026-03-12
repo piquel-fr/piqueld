3f8782d427b24bf09aa664ab21b198590e56f2a6
PiquelChips <piquel@piquel.fr>
2026-03-12 17:46:05 +0100

    refactor error handling in docker service

--- piqueld/src/services/docker.rs
+++ piqueld/src/services/docker.rs
@@ -1,6 +1,6 @@
 use crate::config::ServerConfig;
 use bollard::{Docker, errors::Error, secret::SwarmInitRequest};
-use log::{debug, error, info};
+use log::{debug, info, trace};
 
 pub mod error;
 use error::{DockerError, Result};
@@ -14,32 +14,26 @@ struct DockerService {
 
 #[service(error = DockerError)]
 impl DockerService {
-    async fn init(config: &ServerConfig) -> Result<Self> {
+    async fn init(_config: &ServerConfig) -> Result<Self> {
         let docker = Docker::connect_with_socket_defaults()
             .map_err(|source| DockerError::SocketConnectionError(source))?;
         info!("{PREFIX} Connected docker socket");
 
-        // TODO: error handling & async
         // SWARM
         match docker.inspect_swarm().await {
-            Ok(_) => debug!("{PREFIX} This is a Swarm Node"),
+            Ok(_) => trace!("{PREFIX} This is a Swarm Node"),
             Err(Error::DockerResponseServerError {
                 status_code: _,
                 message: _,
             }) => {
                 info!("{PREFIX} This is not a Swarm Node. Attempting to initialise one...");
-                match docker.init_swarm(SwarmInitRequest::default()).await {
-                    Ok(msg) => info!("{PREFIX} Successfully created swarm: {msg}"),
-                    Err(err) => {
-                        error!("{PREFIX} Unable to initialise Swarm: {err:#}");
-                        return Err(err);
-                    }
-                };
+                let msg = docker
+                    .init_swarm(SwarmInitRequest::default())
+                    .await
+                    .map_err(|err| DockerError::SwarmInitializationError(err))?;
+                info!("{PREFIX} Successfully created swarm: {msg}");
             }
-            Err(err) => {
-                error!("{PREFIX} Unable to detect Swarm: {err:#}");
-                return Err(err);
-            }
+            Err(err) => return Err(DockerError::SwarmDetectionError(err)),
         }
 
         info!("{PREFIX} Initialised Docker service");
--- piqueld/src/services/docker/error.rs
+++ piqueld/src/services/docker/error.rs
@@ -6,4 +6,9 @@ pub type Result<T> = std::result::Result<T, DockerError>;
 pub enum DockerError {
     #[error("failed to connect to docker socket: {0}")]
     SocketConnectionError(#[source] bollard::errors::Error),
+
+    #[error("failed to detect docker swarm: {0}")]
+    SwarmDetectionError(#[source] bollard::errors::Error),
+    #[error("failed to initialize docker swarm: {0}")]
+    SwarmInitializationError(#[source] bollard::errors::Error),
 }
